ee9bc42b6a81d8233342461346b59eb6df78898a
lovewinter <>
2018-03-28 06:38:21 +0800

    Add the validation for personal information update

--- FirebaseApp/PersonalInforViewController.swift
+++ FirebaseApp/PersonalInforViewController.swift
@@ -8,30 +8,24 @@
 
 import UIKit
 import Firebase
-import FirebaseDatabase
-
-
 
 class PersonalInforViewController: UIViewController {
-
-
-    var ref = Database.database().reference()
-
     
+    var ref: DatabaseReference!
     
     @IBOutlet weak var lastNameField: UITextField!
     @IBOutlet weak var ChineseNameField: UITextField!
     @IBOutlet weak var firstNameField: UITextField!
-      @IBOutlet weak var HKID: UITextField!
-      @IBOutlet weak var DateOfBirth: UIDatePicker!
-     @IBOutlet weak var Weight: UITextField!
-       @IBOutlet weak var Height: UITextField!
-     @IBOutlet weak var bloodGroup: UITextField!
+    @IBOutlet weak var HKID: UITextField!
+    @IBOutlet weak var DateOfBirth: UIDatePicker!
+    @IBOutlet weak var Weight: UITextField!
+    @IBOutlet weak var Height: UITextField!
+    @IBOutlet weak var bloodGroup: UITextField!
     @IBOutlet weak var donorID: UITextField!
     @IBOutlet weak var whetherDonatedBlood: UISegmentedControl!
     @IBOutlet weak var lastDonationDate: UIDatePicker!
     @IBOutlet weak var Address: UITextField!
-   @IBOutlet weak var mobileNo: UITextField!
+    @IBOutlet weak var mobileNo: UITextField!
     @IBOutlet weak var email: UITextField!
 
     override func viewDidLoad() {
@@ -40,36 +34,96 @@ class PersonalInforViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
     
-  
     @IBAction func submitBtn(_ sender: UIButton) {
         ref = Database.database().reference()
         
-
-       let uid = Auth.auth().currentUser!.uid
+        let uid = Auth.auth().currentUser!.uid
         let formatter = DateFormatter()
         formatter.dateStyle = .medium
         formatter.timeStyle = .none
         var don = ""
-
+        // check error message
+        var showError = 0
+        var showErrorMessage = ""
         
-        if firstNameField.text != ""{
-                ref.child("user").child(uid).child("firstName").setValue(firstNameField.text)
-                ref.child("user").child(uid).child("lastName").setValue(lastNameField.text)
-                ref.child("user").child(uid).child("chinese_name").setValue(ChineseNameField.text)
-                ref.child("user").child(uid).child("HKID").setValue(HKID.text)
-            
-                let dateOfBirth = formatter.string(from: DateOfBirth.date)
-                ref.child("user").child(uid).child("date_brith").setValue(dateOfBirth)
-                ref.child("user").child(uid).child("weight").setValue(Weight.text)
-                ref.child("user").child(uid).child("height").setValue(Height.text)
-                ref.child("user").child(uid).child("blood_group").setValue(bloodGroup.text)
-            
-                if(whetherDonatedBlood.selectedSegmentIndex == 0){
-                    don = "true"
-                }else{
-                    don = "false"
+        if firstNameField.text != "" && lastNameField.text != "" {
+            showError += 0
+        } else {
+            showError += 1
+            showErrorMessage += "Please Input First Name & Last Name \n"
+        }
+        
+        if isValidHKID(checkHKID: HKID.text!) {
+            showError += 0
+        } else {
+            showError += 1
+            showErrorMessage += "Please Input Correct HKID \n"
+        }
+        if Weight.text != "" && Height.text != "" {
+            showError += 0
+            } else {
+            showError += 1
+            showErrorMessage += "Please Input Weight & Height \n"
+        }
+        if bloodGroup.text == "" || bloodGroup.text == "A" || bloodGroup.text == "B" || bloodGroup.text == "AB" || bloodGroup.text == "O" {
+            showError += 0
+        } else {
+            showError += 1
+            showErrorMessage += "Please Input Correct Blood Group(A/B/AB/O) \n"
+        }
+        
+        if donorID.text == "" || isValidDonorID(checkDonorID: donorID.text!) {
+            showError += 0
+        } else {
+            showError += 1
+            showErrorMessage += "Please Input Correct Donor ID \n"
+        }
+        if Address.text != "" {
+            showError += 0
+        } else {
+            showError += 1
+            showErrorMessage += "Please Input Correct Address \n"
+        }
+        
+        if isValidPhone(checkPhone: mobileNo.text!) {
+            showError += 0
+        } else {
+            showError += 1
+            showErrorMessage += "Please Input Correct Phone Number \n"
+        }
+        
+        if isValidEmail(checkEmail: email.text!) {
+            showError += 0
+        } else {
+            showError += 1
+            showErrorMessage += "Please Input Correct Email \n"
+        }
+        // Update Information or show Error Message
+        if showError != 0 {
+            // show message box
+            let alert = UIAlertController(title: "Failed", message: showErrorMessage, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .`default`, handler: { _ in
+                NSLog("The \"OK\" alert occured.")
+            }))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            ref.child("user").child(uid).child("firstName").setValue(firstNameField.text)
+            ref.child("user").child(uid).child("lastName").setValue(lastNameField.text)
+            ref.child("user").child(uid).child("chinese_name").setValue(ChineseNameField.text)
+            ref.child("user").child(uid).child("HKID").setValue(HKID.text)
+        
+            let dateOfBirth = formatter.string(from: DateOfBirth.date)
+            ref.child("user").child(uid).child("date_brith").setValue(dateOfBirth)
+            ref.child("user").child(uid).child("weight").setValue(Weight.text)
+            ref.child("user").child(uid).child("height").setValue(Height.text)
+            ref.child("user").child(uid).child("blood_group").setValue(bloodGroup.text)
+            ref.child("user").child(uid).child("Donor ID").setValue(donorID.text)
+        
+            if(whetherDonatedBlood.selectedSegmentIndex == 0){
+                don = "true"
+            }else{
+                don = "false"
             }
             ref.child("user").child(uid).child("whether_donated_blood").setValue(don)
             let LastDonationDate = formatter.string(from: lastDonationDate.date)
@@ -77,7 +131,34 @@ class PersonalInforViewController: UIViewController {
             ref.child("user").child(uid).child("address").setValue(Address.text)
             ref.child("user").child(uid).child("mobile_No").setValue(mobileNo.text)
             ref.child("user").child(uid).child("email").setValue(email.text)
-                    }
+            // show message box
+            let alert = UIAlertController(title: "Congratulations", message: "Success to Update.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .`default`, handler: { _ in
+                NSLog("The \"OK\" alert occured.")
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func isValidHKID(checkHKID: String) -> Bool {
+        let HKIDRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let HKIDTest = NSPredicate(format:"SELF MATCHES %@", HKIDRegEx)
+        return HKIDTest.evaluate(with: checkHKID)
+    }
+    func isValidDonorID(checkDonorID: String) -> Bool {
+        let DonorIDRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let DonorIDTest = NSPredicate(format:"SELF MATCHES %@", DonorIDRegEx)
+        return DonorIDTest.evaluate(with: checkDonorID)
+    }
+    func isValidPhone(checkPhone: String) -> Bool {
+        let PhoneRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let PhoneTest = NSPredicate(format:"SELF MATCHES %@", PhoneRegEx)
+        return PhoneTest.evaluate(with: checkPhone)
+    }
+    func isValidEmail(checkEmail: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: checkEmail)
     }
     
 }
